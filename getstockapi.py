b82e2df638c57d7f0a0c8415770a4957311dc450
Aidan Jiang <aidanjiang3@gmail.com>
2026-06-23 17:07:04 +0000

    Got the data from alpaca, you might have to get your own keys

--- getstockapi.py
+++ getstockapi.py
@@ -2,8 +2,31 @@ import requests
 import pandas as pd
 import os
 import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+SECRET = os.getenv("SECRET")
+
+#Gets most active stocks
+#DOCS: https://docs.alpaca.markets/us/reference/mostactives-1
+url_active = "https://data.alpaca.markets/v1beta1/screener/stocks/most-actives"
+
+#Get OCHLV for individual stocks
+#DOCS: https://docs.alpaca.markets/us/reference/stockbars
+url_stock = "https://data.alpaca.markets/v2/stocks/AAPL/bars"
 
 
+params = {
+    "by": "volume",
+    "top": 100
+}
+
+response = requests.get(url_active, params = params, auth = (API_KEY, SECRET))
+data = response.json()
+
+print(data)
 
 
 
