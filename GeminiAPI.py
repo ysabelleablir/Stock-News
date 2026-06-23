01f24c16d64a5e1d72492ba7ddba041d8ddeb96e
Darius Green <dariusg@andrew.cmu.edu>
2026-06-23 18:31:00 +0000

    added a standardized format for the AI prompt

--- GeminiAPI.py
+++ GeminiAPI.py
@@ -6,18 +6,36 @@ from google.genai import types
 load_dotenv()
 
 gemini_key = os.getenv('Gemini_Key')
+ticker = ___
+articles = ___
 
 client = genai.Client(
   api_key=gemini_key,
 )
 
+prompt = f"""
+Stock ticker: {ticker}
+
+News articles:
+
+Article 1:
+{articles[0]}
+
+Article 2:
+{articles[1]}
+
+Article 3:
+{articles[2]}
+
+you are a wall street financial analyst trying to predict a stock's response.
+Based on the above articles given to you Predict:
+weather the stock will go up or down tomorrow.
+"""
+
 interaction = client.interactions.create(
     model="gemini-3.5-flash",
     system_instruction=(
-      "you are a wall street financial analyst trying to predict a "
-      "stock's response based on news articles given to you."
-      ),
-    input="What are the advantages of pair programming?",
+    #input=prompt,
 )
 
 print(interaction.output)
